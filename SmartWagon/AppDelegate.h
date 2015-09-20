95f93df20f393805a18e7549f1219d70d37f726f
Hitesh Vyas <hv5451@rit.edu>
2015-09-20 01:13:52 -0400

    Code push

---
+++ SmartWagon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartWagon
+//
+//  Created by Hitesh Vyas on 9/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartWagon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartWagon
+//
+//  Created by Hitesh Vyas on 9/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
